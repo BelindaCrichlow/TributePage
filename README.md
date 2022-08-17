5bc02a6ce20b10be1267538c660f40feb1cc5941
Belinda Crichlow <crichlowb@gmail.com>
2022-08-17 15:32:46 -0400

    All Files added

---
+++ README.md
@@ -0,0 +1,2 @@
+# TributePage
+ Tribute to Michelle Obama
---
+++ js/tribute.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+(function($) {
+    "use strict";
+    
+function menu_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function menu_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
